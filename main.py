399f4069b69fc3418afa8339aa5bb78c1df949c7
Stanislav Mitev <stanitev@gmail.com>
2023-03-29 20:47:03 +0300

    create the screen

---
+++ main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+# Create and modify the screen
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("My Pong Arcade Game!")
+
+screen.exitonclick()
+
